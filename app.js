bc4573a9cfc0bfd18700eb4b7f13d9148a2ccfdf
Tijs <tijs.martens@student.hogent.be>
2020-06-19 07:57:56 +0200

    Add form validation

--- app.js
+++ app.js
@@ -2,22 +2,67 @@ const createDateString = (day, month, year) => {
   return `${day}/${month}/${year}`;
 };
 
-const displayMessage = (text, backgroundColor = 'red') => {
-  messageBox =  document.getElementById('popupMessage')
-  messageBox.style.backgroundColor=backgroundColor;
+const displayMessage = (text, backgroundColor = "red") => {
+  messageBox = document.getElementById("popupMessage");
+  messageBox.style.backgroundColor = backgroundColor;
   messageBox.innerHTML = text;
 
-  messageBox.style.display = 'block';
+  messageBox.style.display = "block";
   setTimeout(() => {
-    messageBox.style.display='none'
+    messageBox.style.display = "none";
   }, 1000);
-}
+};
 
+const checkEmpty = (text, fieldName) => {
+  if (text === "") {
+    displayMessage(`${fieldName} mag niet leeg zijn.`);
+    return false;
+  }
+  return true;
+};
+
+const checkValidateEmail = (mail) => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  } else {
+    displayMessage(`geen geldig email`);
+    return false;
+  }
+};
+
+const checkDate = (_day, _month, _year) => {
+  const day = parseInt(_day, 10);
+  const month = parseInt(_month, 10);
+  const year = parseInt(_year, 10);
+
+  console.log(day);
+  console.log(month);
+  console.log(year);
+
+  if (isNaN(day) || day > 31 || day < 1) {
+    displayMessage("dag moet een getal tussen 1 en 31 zijn");
+    return false;
+  }
+
+  if (isNaN(month) || month > 12 || month < 1) {
+    displayMessage("maand moet een getal tussen 1 en 12 zijn");
+    return false;
+  }
+
+  if (isNaN(year) || year > 2020 || year < 1900) {
+    displayMessage("Vul een geldig jaartal in");
+    return false;
+  }
+
+  return true;
+};
 
 document
   .getElementById("registerKidButton")
   .addEventListener("click", (event) => {
     event.preventDefault();
+
+    let formIsValid = true;
 
     const kidname = document.getElementById("kidName").value;
     const kidEmail = document.getElementById("kidEmail").value;
@@ -25,25 +70,32 @@ document
     const kidMonth = document.getElementById("kidMonth").value;
     const kidYear = document.getElementById("kidYear").value;
 
-    displayMessage('test')
+    !checkEmpty(kidname, "naam") ? (formIsValid = false) : null;
+    !checkEmpty(kidEmail, "email") ? (formIsValid = false) : null;
+    !checkEmpty(kidDay, "geboortedatum") ? (formIsValid = false) : null;
+    !checkEmpty(kidMonth, "geboortedatum") ? (formIsValid = false) : null;
+    !checkEmpty(kidYear, "geboortedatum") ? (formIsValid = false) : null;
 
+    !checkValidateEmail(kidEmail) ? (formIsValid = false) : null;
+    !checkDate(kidDay, kidMonth, kidYear) ? (formIsValid = false) : null;
 
-
-
-    saveInAirtable("Kinderen", {
-      fields: {
-        Naam: kidname,
-        Email: kidEmail,
-        Geboortedatum: createDateString(kidDay, kidMonth, kidYear),
-      },
-    });
+    if(formIsValid){
+      saveInAirtable("Kinderen", {
+        fields: {
+          Naam: kidname,
+          Email: kidEmail,
+          Geboortedatum: createDateString(kidDay, kidMonth, kidYear),
+        },
+      });
+    }
   });
 
 document
   .getElementById("registerRetroButton")
   .addEventListener("click", (event) => {
-    console.log("yeet 2");
     event.preventDefault();
+
+    let formIsValid = true;
 
     const retroName = document.getElementById("retroName").value;
     const retroEmail = document.getElementById("retroEmail").value;
@@ -52,12 +104,23 @@ document
     const retroyear = document.getElementById("retroYear").value;
     const retroIsLocal = document.getElementById("retroIsLocal").checked;
 
-    saveInAirtable("Retrokoers", {
-      fields: {
-        Naam: retroName,
-        Email: retroEmail,
-        Geboortedatum: createDateString(retroDay, retroMonth, retroyear),
-        Dikkelvennenaar: retroIsLocal
-      },
-    });
+    !checkEmpty(retroName, "naam") ? (formIsValid = false) : null;
+    !checkEmpty(retroEmail, "email") ? (formIsValid = false) : null;
+    !checkEmpty(retroDay, "geboortedatum") ? (formIsValid = false) : null;
+    !checkEmpty(retroMonth, "geboortedatum") ? (formIsValid = false) : null;
+    !checkEmpty(retroyear, "geboortedatum") ? (formIsValid = false) : null;
+
+    !checkValidateEmail(retroEmail) ? (formIsValid = false) : null;
+    !checkDate(retroDay, retroMonth, retroyear) ? (formIsValid = false) : null;
+
+    if(formIsValid){
+      saveInAirtable("Retrokoers", {
+        fields: {
+          Naam: retroName,
+          Email: retroEmail,
+          Geboortedatum: createDateString(retroDay, retroMonth, retroyear),
+          Dikkelvennenaar: retroIsLocal,
+        },
+      });
+    }
   });
